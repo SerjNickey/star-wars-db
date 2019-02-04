2a9f83d5765595fc2645b0ec3cb3991178949175
SerjNickey <serjnickey@gmail.com>
2019-02-04 20:03:11 +0300

    Commit 3 (Random Planet interval to 10000)

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -14,7 +14,7 @@ export default class RandomPlanet extends Component {
 
   componentDidMount() {
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 5000);
+    this.interval = setInterval(this.updatePlanet, 10000);
   };
 
   componentWillUnmount() {
